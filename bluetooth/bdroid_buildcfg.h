476842260b44b86f9617b850014837fd1b2a9318
kyasu <kyasu.dev@gmail.com>
2016-07-24 03:20:42 +0900

    js01ltedcm: Set correct bluetooth name

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 The CyanogenMod Project
+ * Copyright (C) 2016 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Samsung Galaxy Note 3"
+#define BTM_DEF_LOCAL_NAME   "Samsung Galaxy J (SC-02F)"
 
 #define BTA_DISABLE_DELAY 100 /* in milliseconds */
 #define BTM_WBS_INCLUDED TRUE
