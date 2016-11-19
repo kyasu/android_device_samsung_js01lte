a99b11c946ff264df284a6a89c71d5229b0b6173
kyasu <kyasu.dev@gmail.com>
2016-11-19 10:04:09 +0900

    js01lte: Remove enhanced NFC feature comment

--- cm.mk
+++ cm.mk
@@ -1,8 +1,5 @@
 $(call inherit-product, device/samsung/js01lte/full_js01lte.mk)
 
-# Enhanced NFC
-#$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
