8ea48e6cf6c109b0af39d5842eeaced195f72e28
kyasu <kyasu.dev@gmail.com>
2017-11-18 15:52:25 +0900

    js01lte: Clean up BT settings
    * Force SCO WBS samplerate to 8kHz

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -24,7 +24,4 @@
 #define BTIF_HF_WBS_PREFERRED TRUE
 #define BLE_VND_INCLUDED TRUE
 
-#define BTM_WBS_INCLUDED TRUE       /* Enable WBS */
-#define BTIF_HF_WBS_PREFERRED TRUE  /* Use WBS    */
-
 #endif
