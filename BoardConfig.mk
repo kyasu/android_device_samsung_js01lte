fcd3d2d3954aca753b80312cfbc03414763e8a16
kyasu <kyasu.dev@gmail.com>
2018-01-27 08:02:11 +0900

    Revert "js01lte: Add AID_QCOM_DIAG, AID_RFS, and AID_RFS_SHARED to config.fs"
    This reverts commit aa6daff9d5174fb7b50649bbc79c1ea6d03da5d8.
    
    * Move this to msm8974-common

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -74,9 +74,6 @@ USE_DEVICE_SPECIFIC_CAMERA := true
 # Extended Filesystem Support
 TARGET_KERNEL_HAVE_EXFAT := true
 
-# Filesystem
-TARGET_FS_CONFIG_GEN := device/samsung/js01lte/config.fs
-
 # HIDL
 DEVICE_MANIFEST_FILE += device/samsung/js01lte/manifest.xml
 
