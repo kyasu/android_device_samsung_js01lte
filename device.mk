2141c4cd43183c029033c425c014402317d9be9b
kyasu <kyasu.dev@gmail.com>
2021-05-12 10:57:17 +0900

    js01lte: Don't let builds complete without vendor tree
    * Also add msm8974-common vendor repo in README.

--- device.mk
+++ device.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2014 The CyanogenMod Project
+# Copyright (C) 2017-2021 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -20,7 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, device/samsung/js01lte/msm8974-common/msm8974.mk)
 
 # Get non-open-source specific aspects
-$(call inherit-product-if-exists, vendor/samsung/js01lte/js01lte-vendor.mk)
+$(call inherit-product, vendor/samsung/js01lte/js01lte-vendor.mk)
 
 
 # Overlays
